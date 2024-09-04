3495729ac9eb9001c0c770aca30495d8653b2ffc
Roman Kalyakin <roman@kalyakin.com>
2024-09-04 17:31:01 +0200

    added healthcheck and autoheal

---
+++ scripts/nginx-healthcheck.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Successful conditions:
+# Both of these pages should not return 404. If they do - it's a problem and Nginx should be restarted.
+$(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/public-api/search) -ne 404) || \
+$(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/api/socket.io/) -ne 404)
