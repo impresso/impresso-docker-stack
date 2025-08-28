7412207c8de429b0a997b6bb59461985528c7670
Roman Kalyakin <roman@kalyakin.com>
2025-08-28 07:49:19 +0700

    added production configuration (#16)

---
+++ config_prod/recsys_config.py
@@ -0,0 +1,16 @@
+import urllib.parse
+from .settings import *
+import os
+
+DEBUG = True
+CORS_ORIGIN_WHITELIST = []
+ALLOWED_HOSTS = "*"
+
+SOLR_USERNAME = urllib.parse.quote(os.environ.get("SOLR_READER_USERNAME"))
+SOLR_PASSWORD = urllib.parse.quote(os.environ.get("SOLR_READER_PASSWORD"))
+SOLR_BASE_URL_NO_AUTH = os.environ.get("SOLR_BASE_URL")
+SOLR_BASE_URL = SOLR_BASE_URL_NO_AUTH.replace(
+    "://", f"://{SOLR_USERNAME}:{SOLR_PASSWORD}@"
+)
+
+SOLR_TOPICS_COLLECTION = "02_impresso_topics"
