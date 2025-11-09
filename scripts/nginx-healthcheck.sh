d11f4c59f89ecf1fa8036c2dc717974db6adedfa
Roman Kalyakin <roman@kalyakin.com>
2025-11-09 17:06:08 +0100

    install curl in healthcheck if not there

--- scripts/nginx-healthcheck.sh
+++ scripts/nginx-healthcheck.sh
@@ -2,5 +2,6 @@
 
 # Successful conditions:
 # Both of these pages should not return 404. If they do - it's a problem and Nginx should be restarted.
+curl -V || apk add --no-cache curl
 $(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/public-api/search) -ne 404) || \
 $(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/api/socket.io/) -ne 404)
