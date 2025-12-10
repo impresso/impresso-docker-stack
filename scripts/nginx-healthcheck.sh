2c538292b7fbd1585b16dbad4252489353f888ad
Roman Kalyakin <roman@kalyakin.com>
2025-12-10 16:35:46 +0100

    healthcheck endpoint updated

--- scripts/nginx-healthcheck.sh
+++ scripts/nginx-healthcheck.sh
@@ -3,5 +3,5 @@
 # Successful conditions:
 # Both of these pages should not return 404. If they do - it's a problem and Nginx should be restarted.
 curl -V || apk add --no-cache curl
-$(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/public-api/search) -ne 404) || \
+$(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/public-api/v1/search) -ne 404) || \
 $(test $(curl --connect-timeout 3 -s -o /dev/null -w "%{http_code}" http://localhost/api/socket.io/) -ne 404)
